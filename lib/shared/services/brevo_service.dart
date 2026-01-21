1f19a7d09aa499fb299e94e861a487ac2f45c84f
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-01-21 03:44:47 +0300

    change api key

--- lib/shared/services/brevo_service.dart
+++ lib/shared/services/brevo_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class BrevoService {
   final String brevoApiKey =
-      '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';
+      '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';
 
   Future<void> sendEmail({
     required String toEmail,
